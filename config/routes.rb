b05bd36efb70686f6112f14ba0cdb3ecf53371fb
Thierry Oliveira <thierry_fernando@hotmail.com>
2019-08-20 16:47:04 -0300

    Esqueci de commitar antes, acabei tudo =D

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,24 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # 1. index
-  # 2. new
-  # 3. create
+  get '/home', to: 'tasks#index'
+  # 1. new
+  get '/tasks/new', to: 'tasks#new'
+
+  # 2. create
+  post '/tasks', to: 'tasks#create'
+
+  # 3. index
+  get '/tasks', to: 'tasks#index'
+
   # 4 .show
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
+
   # 5. edit
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit'
+
   # 6. update
+  patch '/tasks/:id', to: 'tasks#update'
+
   # 7. destroy
+  delete '/tasks/:id', to: 'tasks#destroy'
 end
