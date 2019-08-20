3cdd04251735a5937d1b69255c2119938425e972
Thierry Oliveira <thierry_fernando@hotmail.com>
2019-08-20 12:20:26 -0300

    Add Title, Details e Completed to Task

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # 1. index
+  # 2. new
+  # 3. create
+  # 4 .show
+  # 5. edit
+  # 6. update
+  # 7. destroy
 end
